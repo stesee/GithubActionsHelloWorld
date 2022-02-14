0deb7f38aa4fc77355785a931f173ead59390946
Stefan Seeland <stefan.seeland@rubicon.eu>
2022-02-14 17:23:49 +0100

    Added ZXing Poc

--- GithubActionsHelloWorld/Program.cs
+++ GithubActionsHelloWorld/Program.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using System.Threading.Tasks;
+using ZXing.Common;
+using ZXing.Rendering;
 
 namespace GithubActionsHelloWorld
 {
     public class Program
     {
-        public async static Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            await Task.Delay(100);
-            Console.WriteLine("Hello World!");
+            var barcodeWriter = new ZXing.BarcodeWriterPixelData
+            {
+                Format = ZXing.BarcodeFormat.CODE_128,
+                Options = new EncodingOptions
+                {
+                    Height = 300,
+                    Width = 300,
+                },
+                Renderer = new PixelDataRenderer
+                {
+                }
+            };
+
+            var pixelData = barcodeWriter.Write("Hallo Barcode");
+
+            using (var image = Image.LoadPixelData<Rgba32>(pixelData.Pixels, 300, 300))
+                await image.SaveAsBmpAsync("barcode.bmp");
         }
     }
 }
