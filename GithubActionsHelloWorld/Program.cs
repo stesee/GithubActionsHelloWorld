11ee8e0023698d4ca29d3ee4a9860f32413b9fe5
Stefan Seeland <stefan.seeland@rubicon.eu>
2022-09-26 16:58:54 +0200

    Poc Multipage Tiff to Pdf

--- GithubActionsHelloWorld/Program.cs
+++ GithubActionsHelloWorld/Program.cs
@@ -1,8 +1,10 @@
-﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
+﻿using PdfSharpCore.Drawing;
+using PdfSharpCore.Fonts;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Utils;
+using SixLabors.ImageSharp;
+using System.IO;
 using System.Threading.Tasks;
-using ZXing.Common;
-using ZXing.Rendering;
 
 namespace GithubActionsHelloWorld
 {
@@ -10,23 +12,41 @@ namespace GithubActionsHelloWorld
     {
         public static async Task Main(string[] args)
         {
-            var barcodeWriter = new ZXing.BarcodeWriterPixelData
-            {
-                Format = ZXing.BarcodeFormat.CODE_128,
-                Options = new EncodingOptions
+            GlobalFontSettings.FontResolver = new FontResolver();
+
+            var document = new PdfDocument();
+
+            using (var tiff = Image.Load(args[0]))
+
+                for (var pageIndex = 0; pageIndex < tiff.Frames.Count; pageIndex++)
                 {
-                    Height = 300,
-                    Width = 300,
-                },
-                Renderer = new PixelDataRenderer
-                {
+                    var pageImage = await ConvertToXImageAsync(tiff.Frames.CloneFrame(pageIndex));
+                    var pageWithImage = new PdfPage();
+                    document.Pages.Add(pageWithImage);
+                    var xgr = XGraphics.FromPdfPage(document.Pages[pageIndex]);
+                    xgr.DrawImage(pageImage, 0, 0);
                 }
-            };
 
-            var pixelData = barcodeWriter.Write("Hallo Barcode");
+            var page = document.AddPage();
+            var gfx = XGraphics.FromPdfPage(page);
+            var font = new XFont("Arial", 20, XFontStyle.Bold);
 
-            using (var image = Image.LoadPixelData<Rgba32>(pixelData.Pixels, 300, 300))
-                await image.SaveAsBmpAsync("barcode.bmp");
+            var textColor = XBrushes.Black;
+            var layout = new XRect(20, 20, page.Width, page.Height);
+            var format = XStringFormats.Center;
+
+            gfx.DrawString("Hello World!", font, textColor, layout, format);
+
+            document.Save("helloworld.pdf");
+        }
+
+        private static async Task<XImage> ConvertToXImageAsync(Image imageFrame)
+        {
+            using var memoryStream = new MemoryStream();
+            await imageFrame.SaveAsPngAsync(memoryStream);
+            memoryStream.Position = 0;
+            var image = XImage.FromStream(() => memoryStream);
+            return image;
         }
     }
 }
--- GithubActionsHelloWorldTests/GithubActionsHelloWorldTest.cs
+++ GithubActionsHelloWorldTests/GithubActionsHelloWorldTest.cs
@@ -1,4 +1,8 @@
 using GithubActionsHelloWorld;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -6,11 +10,30 @@ namespace GithubActionsHelloWorldTests
 {
     public class GithubActionsHelloWorldTest
     {
+        private const string expectedCreatedFilepath = "helloworld.pdf";
+
         [Fact]
         public async Task Main_ShouldRunWithoutError()
         {
-            var args = new string[] { "SomeParameter" };
+            var codeBaseUrl = new Uri(Assembly.GetExecutingAssembly().Location);
+            var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath);
+            var dirPath = Path.GetDirectoryName(codeBasePath);
+            var multipageTiff = Path.GetFullPath(Path.Combine(dirPath, "../../../MultiPage.tiff"));
+
+            if (File.Exists(expectedCreatedFilepath))
+            {
+                File.Delete(expectedCreatedFilepath);
+            }
+
+            var args = new string[] { multipageTiff };
             await Program.Main(args);
+
+            Assert.True(File.Exists(expectedCreatedFilepath));
+
+            using var process = new Process();
+            process.StartInfo.FileName = expectedCreatedFilepath;
+            process.StartInfo.UseShellExecute = true;
+            process.Start();
         }
     }
 }
