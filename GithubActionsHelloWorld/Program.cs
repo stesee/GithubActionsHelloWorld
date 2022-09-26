68e48808fcde0788dc316ceea1fece8d2899677b
Stefan Seeland <stefan.seeland@rubicon.eu>
2022-09-26 20:33:55 +0200

    Dropped dead code

--- GithubActionsHelloWorld/Program.cs
+++ GithubActionsHelloWorld/Program.cs
@@ -108,49 +108,5 @@ namespace GithubActionsHelloWorld
 
             return pageCount;
         }
-
-        public static SKBitmap OpenTiff(Stream tiffStream)
-        {
-            // https://stackoverflow.com/questions/50312937/skiasharp-tiff-support
-
-            tiffStream.Position = 0;
-            // open a TIFF stored in the stream
-            using (var tifImg = Tiff.ClientOpen("in-memory", "r", tiffStream, new TiffStream()))
-            {
-                tifImg.SetDirectory(1);
-
-                // read the dimensions
-                var width = tifImg.GetField(TiffTag.IMAGEWIDTH)[0].ToInt();
-                var height = tifImg.GetField(TiffTag.IMAGELENGTH)[0].ToInt();
-
-                // create the bitmap
-                var bitmap = new SKBitmap();
-                var info = new SKImageInfo(width, height);
-
-                // create the buffer that will hold the pixels
-                var raster = new int[width * height];
-
-                // get a pointer to the buffer, and give it to the bitmap
-                var ptr = GCHandle.Alloc(raster, GCHandleType.Pinned);
-                bitmap.InstallPixels(info, ptr.AddrOfPinnedObject(), info.RowBytes, null, (addr, ctx) => ptr.Free(), null);
-
-                // read the image into the memory buffer
-                if (!tifImg.ReadRGBAImageOriented(width, height, raster, Orientation.TOPLEFT, true))
-                {
-                    // not a valid TIF image.
-                    return null;
-                }
-
-                // swap the red and blue because SkiaSharp may differ from the tiff
-                if (SKImageInfo.PlatformColorType == SKColorType.Bgra8888)
-                {
-                    SKSwizzle.SwapRedBlue(ptr.AddrOfPinnedObject(), raster.Length);
-                }
-
-                File.WriteAllBytes("test.bmp", bitmap.Bytes);
-
-                return bitmap;
-            }
-        }
     }
 }
