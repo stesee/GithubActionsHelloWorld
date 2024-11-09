0b3aca69da8f02ef7d97cc59ab4641ef135ee92e
Stefan Seeland <168659+stesee@users.noreply.github.com>
2024-11-09 17:02:34 +0100

    datetime to string experiment

--- GithubActionsHelloWorldTests/GithubActionsHelloWorldTest.cs
+++ GithubActionsHelloWorldTests/GithubActionsHelloWorldTest.cs
@@ -1,4 +1,6 @@
 using GithubActionsHelloWorld;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -11,6 +13,18 @@ namespace GithubActionsHelloWorldTests
         {
             var args = new string[] { "SomeParameter" };
             await Program.Main(args);
+        }
+
+        [Fact]
+        public void ShouldDoTheSameThingOnEachGHARunner()
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+            string expected = "9/2/2010 12:00:00 AM";
+            DateTime parsedDate = DateTime.Parse(expected);
+            var actual = parsedDate.ToString();
+
+            Assert.Equal(expected, actual);
         }
     }
 }
